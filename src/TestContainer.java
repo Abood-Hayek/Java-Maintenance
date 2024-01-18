08f18850adcf5834ea58c073ee216e0eb4f19827
IurEux <229341780@qq.com>
2024-01-19 00:18:07 +0800

    sazid 2

--- src/TestContainer.java
+++ src/TestContainer.java
@@ -5,7 +5,7 @@ public class TestContainer {
     public static void main(String... args)
     {
         Ball ball = new Ball(50, 50, 5, 10, 30);
-        Container box = new Container(0, 0, 100, 100);
+        BallContainer box = new BallContainer(0, 0, 100, 100);
         for (int step = 0; step < 100; ++step) {
            ball.move();
            box.collidesWith(ball);
